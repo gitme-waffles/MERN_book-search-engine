c7cd5c9f7a6a26b173a720cc5414d7e1c566198f
dossAnania <85494162+dossAnania@users.noreply.github.com>
2022-01-23 00:30:47 +1100

    schema done

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,12 +1,42 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-      _id: ID
-      username: String
-      email: String
-      password: String
-      savedBooks: 
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Integer
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID!
+    authors: [String!]!
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  input SaveBookInput {
+    bookAuthor: [String!]!
+    description: String!
+    title: String!
+    bookId: String!
+    image: String!
+    link: String!
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SaveBookInput): User
+    removeBook(bookId: String!): User
   }
 `;
 
